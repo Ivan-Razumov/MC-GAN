7957ceebeb0b26389facc8ad6ff5d3090c85be25
razzumoff <razumov.is@phystech.edu>
2020-04-25 12:28:54 +0300

    add loss print and change devicename

--- train.py
+++ train.py
@@ -37,7 +37,7 @@ for epoch in range(1, opt.niter + opt.niter_decay + 1):
         if total_steps % opt.print_freq == 0:
             errors = model.get_current_errors()
             t = (time.time() - iter_start_time) / opt.batchSize
-            #visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+            print(epoch, epoch_iter, errors, t)
             #if opt.display_id > 0:
                 #visualizer.plot_current_errors(epoch, float(epoch_iter)/dataset_size, opt, errors)
 
