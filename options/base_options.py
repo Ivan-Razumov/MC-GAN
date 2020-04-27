e0f481126243c0f22f354df3ba7fa9109f36a6a9
razzumoff <razumov.is@phystech.edu>
2020-04-27 19:30:03 +0300

    set default options

--- options/base_options.py
+++ options/base_options.py
@@ -16,9 +16,9 @@ class BaseOptions():
     def initialize(self):
         #self.parser.add_argument('--dataroot', required=True, help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
         self.parser.add_argument('--dataroot', default='./datasets/Capitals64', help='path to images (should have subfolders trainA, trainB, valA, valB, etc)')
-        self.parser.add_argument('--batchSize', type=int, default=1, help='input batch size')
-        self.parser.add_argument('--loadSize', type=int, default=286, help='scale images to this size')
-        self.parser.add_argument('--fineSize', type=int, default=256, help='then crop to this size')
+        self.parser.add_argument('--batchSize', type=int, default=64, help='input batch size')
+        self.parser.add_argument('--loadSize', type=int, default=64, help='scale images to this size')
+        self.parser.add_argument('--fineSize', type=int, default=64, help='then crop to this size')
         self.parser.add_argument('--input_nc', type=int, default=26, help='# of input image channels')
         self.parser.add_argument('--output_nc', type=int, default=26, help='# of output image channels')
         self.parser.add_argument('--grps', type=int, default=26, help='# of input groups')
